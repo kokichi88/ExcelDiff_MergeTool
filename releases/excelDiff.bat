f92e8e2f1b234ecfbe9fdce261de19a79422bdc1
hungnm88 <kokichi88@gmail.com>
2017-01-23 17:07:40 +0700

    fix minor bug

--- releases/excelDiff.bat
+++ releases/excelDiff.bat
@@ -3,8 +3,8 @@
 @ECHO 1: %1
 @ECHO 2: %2
 
-SET JAVA_HOME = "C:\Program Files\Java\jre1.8.0_121\bin\java.exe"
-SET PATH_2_JAR = "ExcelDiff.jar"
+SET JAVA_HOME="C:\Program Files\Java\jre1.8.0_121\bin\java.exe"
+SET PATH_2_JAR="ExcelDiff.jar"
 
 SET "FILE1=%1"
 SET "FILE2=%2"
