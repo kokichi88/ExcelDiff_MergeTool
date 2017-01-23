f932fa6a7a9a6053817dd8390a17c38de29990bd
hungnm88 <kokichi88@gmail.com>
2017-01-23 15:00:22 +0700

    update demo runable scripts

--- releases/excelDiff.bat
+++ releases/excelDiff.bat
@@ -2,7 +2,11 @@
 @ECHO Comparing two files:
 @ECHO 1: %1
 @ECHO 2: %2
+
 SET JAVA_HOME = "C:\Program Files\Java\jre1.8.0_121\bin\java.exe"
 SET PATH_2_JAR = "ExcelDiff.jar"
 
-%JAVA_HOME% -jar %PATH_2_JAR% file1=%1 file2=%2
+SET "FILE1=%1"
+SET "FILE2=%2"
+
+%JAVA_HOME% -jar %PATH_2_JAR% file1=%FILE1% file2=%FILE2%
--- releases/excelDiff.sh
+++ releases/excelDiff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_73.jdk/Contents/Home"
 PATH_2_JARFILE="ExcelDiff.jar"
-java -jar ${PATH_2_JARFILE} file1=$1 file2=$2
+${JAVA_HOME}/bin/java -jar ${PATH_2_JARFILE} file1="$1" file2="$2"
